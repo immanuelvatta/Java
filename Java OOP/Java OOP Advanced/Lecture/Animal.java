73b7339311a5e48f742c56900108c21298026cf1
immanuelvatta <immanuelvatta@gmail.com>
2023-12-25 16:32:55 -0600

    animal

---
+++ Java OOP/Java OOP Advanced/Lecture/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+
+    private boolean sleeping = false;
+
+    public void startSleeping() {
+        sleeping = true;
+        System.out.println("ZzZz");
+    }
+
+    public boolean isSleeping() {
+        return sleeping;
+    }
+}
